68af75dc9d069f051a97ee8812fcee1fdfb7f90e
Rahil <rahilalthaf2001@gmail.com>
2026-08-08 13:00:05 +0530

    Add program1.c to calculate sum and product

---
+++ Grade 8/program1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+ int num1 = 10;
+ int num2 = 5;
+ int sum, product;
+ sum = num1 + num2 ;
+ printf(“Sum= %d\n”, sum);
+ product = num1* num2;
+ printf(“Product= %d\n”, product);
+}
